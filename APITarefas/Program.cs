0618a99607c0dbcbd4c6a391f051d9fa6eb275fb
Belarmino Nicolau Monteiro Simão <belarnicolau@hotmail.com>
2022-10-21 07:39:45 -0300

    define endpoint /tarefas as async method

--- APITarefas/Program.cs
+++ APITarefas/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add database to context
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDB"));
 
 var app = builder.Build();
 
@@ -20,6 +25,11 @@ app.MapGet("frases", async () =>
 await new HttpClient().GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
 );
 
+app.MapGet("/tarefas", async (AppDbContext db) =>
+{
+    return await db.Tarefas.ToListAsync();
+});
+
 app.Run();
 
 class Tarefa
@@ -27,4 +37,16 @@ class Tarefa
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+
 }
